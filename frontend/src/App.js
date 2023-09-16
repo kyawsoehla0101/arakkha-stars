e95cc20286f11180a8911bece4f88f3761541d7d
Kyaw Soe Hla <codewarrior.ksh@gmail.com>
2023-09-16 09:21:14 +0630

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './bootstrap.css';
+
+function App() {
+  return (
+    <div className='container py-5'>
+      <h1 className="text-danger">hello world</h1>
+      <button className='btn btn-primary'>Hover Me !</button>
+    </div>
+  );
+}
+
+export default App;
