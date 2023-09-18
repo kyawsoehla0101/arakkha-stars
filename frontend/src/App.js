7c16842804190db1e2df5cdae32523d0520f94ac
Kyaw Soe Hla <codewarrior.ksh@gmail.com>
2023-09-18 08:33:02 +0630

    Add Folder Structure in Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
+import React from "react";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './bootstrap.css';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
-    <div className='container py-5'>
-      <h1 className="text-danger">hello world</h1>
-      <button className='btn btn-primary'>Hover Me !</button>
-    </div>
+    <Router>
+     {/* <Header /> */}
+     <main className="py-3">
+       <Container>
+         <Routes>
+           <Route path="/" element={<HomeScreen />} exact />
+         </Routes>
+       </Container>
+     </main>
+     {/* <Footer /> */}
+   </Router>
   );
 }
 
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HomeScreen() {
+  return (
+    <div className='text-center py-5'>
+      
+      <h1 className="text-danger">Home Screen is the first Page</h1>
+    </div>
+  )
+}
+
+export default HomeScreen
